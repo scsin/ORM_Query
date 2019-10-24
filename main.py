019fb98fe46cdd3d1da9659eeef46da3f9f38466
Sara Sin <saracsin26@gmail.com>
2019-10-24 17:56:52 -0300

    submit ok

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
-# from django.contrib.auth.models import User, Group
 from api.models import Agent, Event, User, Group
-
+import datetime
 
 def get_active_users() -> User:
-    """Traga todos os uarios ativos, seu último login deve ser menor que 10 dias """
-    return User.objects.filter(last_login__contains=2019)
+    today = datetime.date.today()
+    then_days_ago = datetime.timedelta(days=10)
+    return User.objects.filter(last_login__gte=today-then_days_ago)
 
 
 def get_amount_users() -> User:
-    """Retorne a quantidade total de usuarios do sistema """
     return len(User.objects.all())
 
 
 def get_admin_users() -> User:
-    """Traga todos os usuarios com grupo = 'admin"""
     return User.objects.filter(group=1)
 
 
 def get_all_debug_events() -> Event:
-    """Traga todos os eventos com tipo debug"""
     return Event.objects.filter(level__contains='debug')
 
 
 def get_all_critical_events_by_user(agent) -> Event:
-    """Traga todos os eventos do tipo critico de um usuário específico"""
     return Event.objects.filter(level__contains='critical')
 
 
 def get_all_agents_by_user(username) -> Agent:
-    """Traga todos os agentes associados a um usuário pelo nome do usuário"""
     return Agent.objects.filter(user_id__name__contains=username)
 
 
 def get_all_events_by_group() -> Group:
-    """Traga todos os grupos que contenham alguem que possua um agente que possuem eventos do tipo information"""
     return Group.objects.filter(user__agent__event__level__contains='information')
